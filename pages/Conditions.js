ef168f3d7456c092350db393c192003928fb417d
Christopher Makombe <113990481+SwiftlyBox@users.noreply.github.com>
2022-09-21 15:38:56 +0200

    Update Conditions.js

--- pages/Conditions.js
+++ pages/Conditions.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Terms from "../src/components/Terms";
+import Footer from "../src/components/Footer";
+import NavBar from "../src/components/NavBar";
 
 const Conditions = () => {
   return (
     <>
+    <NavBar/>
       <Terms />
+    <Footer/>
     </>
   );
 };
