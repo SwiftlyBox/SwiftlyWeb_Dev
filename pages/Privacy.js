26590b00675c11276c607cc4d6c6aa251474e375
Christopher Makombe <113990481+SwiftlyBox@users.noreply.github.com>
2022-09-21 15:37:44 +0200

    Update Privacy.js

--- pages/Privacy.js
+++ pages/Privacy.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Privacy from "../src/components/Privacy";
+import NavBar from "../src/components/NavBar";
+import Privacy from "../src/components/NavBar";
+
 
 const Conditions = () => {
   return (
     <>
+    <NavBar/>
       <Privacy />
+    <Footer/>
     </>
   );
 };
