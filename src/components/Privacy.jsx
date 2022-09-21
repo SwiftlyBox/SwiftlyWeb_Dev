d6c0289ec1005fe2a69dba3f354480ad83026ef1
Christopher Makombe <113990481+SwiftlyBox@users.noreply.github.com>
2022-09-21 16:35:25 +0200

    Update Privacy.jsx

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Privacy = ()=>{
     return (
-        <>
+        
         <h1>Privacy Policy for SwiftlyBox</h1>
 
 <p>At SwiftlyBox, accessible from {`https://swiftlybox.com`}, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by SwiftlyBox and how we use it.</p>
@@ -80,7 +80,6 @@ const Privacy = ()=>{
 <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
 <p>SwiftlyBox does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
-</>
         
     )
 }
